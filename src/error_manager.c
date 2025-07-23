0acf838a9f4e6fd15eb4012bfbf27ea2e14deabc
Rafael Heringer <rafaelheringer@MacBook-Air-de-Rafael.local>
2025-07-23 14:59:40 -0300

    open_screen

--- src/error_manager.c
+++ src/error_manager.c
@@ -1,36 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   error_manager.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rafaelheringer <rafaelheringer@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/22 18:43:05 by rheringe          #+#    #+#             */
-/*   Updated: 2025/07/23 14:54:08 by rafaelherin      ###   ########.fr       */
+/*   Created: 2025/07/23 11:46:49 by rafaelherin       #+#    #+#             */
+/*   Updated: 2025/07/23 14:53:14 by rafaelherin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int	main(int argc, char **argv)
+void    error_messages(short error_code)
 {
-	t_game	*game;
+    if (error_code == EXIT_TOO_MANY_ARGUMENTS)
+        ft_printf_fd(STDERR_FILENO, "Error\nToo many arguments\n");
+    if (error_code == EXIT_FEW_ARGUMENTS)
+        ft_printf_fd(STDERR_FILENO, "Error\nToo few arguments\n");
 
-	if (argv)
-	{
-	}
-	if (argc > 2)
-	{
-		error_messages(EXIT_TOO_MANY_ARGUMENTS);
-		return (EXIT_TOO_MANY_ARGUMENTS);
-	}
-	if (argc < 2)
-	{
-		error_messages(EXIT_FEW_ARGUMENTS);
-		return (EXIT_FEW_ARGUMENTS);
-	}
-	game = ft_calloc(1, sizeof(t_game));
-	if (!game)
-		error_messages(EXIT_ERROR_MEMORY_ALLOCATION);
-	init_cub3d(game);
 }
+
