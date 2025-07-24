06a95183c5480a590f1368c6c3e520f512405033
Roberto Del Franco <robertinhodf@gmail.com>
2025-07-24 12:15:57 -0300

    new ideas for parser

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rheringe <rheringe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 17:52:53 by rheringe          #+#    #+#             */
-/*   Updated: 2025/07/22 18:26:49 by rheringe         ###   ########.fr       */
+/*   Updated: 2025/07/24 11:58:18 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,8 @@ typedef struct s_texture
 	mlx_texture_t	*east_wall;
 	char			*west_path;
 	mlx_texture_t	*west_wall;
+	char			*floor_color;
+	char			*ceiling_color;
 }	t_texture;
 
 typedef struct s_game
