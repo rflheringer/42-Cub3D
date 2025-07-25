bbcccbb3f279bab4ac01516a84328092ee626a7c
Rafael <rafael.heringer@hotmail.com>
2025-07-25 18:38:33 -0300

    screen printing

--- src/initializers.c
+++ src/initializers.c
@@ -6,13 +6,13 @@
 /*   By: rheringe <rheringe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 14:41:18 by rafaelherin       #+#    #+#             */
-/*   Updated: 2025/07/25 17:43:19 by rheringe         ###   ########.fr       */
+/*   Updated: 2025/07/25 18:15:49 by rheringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void	static init_map_images(t_game *game)
+static void	init_map_images(t_game *game)
 {
 	game->texture->south_wall = mlx_load_png("assets/map/dungeon_wall_1.png");
 	if (!game->texture->south_wall)
@@ -22,10 +22,12 @@ void	static init_map_images(t_game *game)
 	if (!game->image->south_wall)
 		error_messages(EXIT_FAILED_TO_LOAD_IMAGE);
 }
+
 int32_t	init_cub3d(t_game *game)
 {
 	game->mlx = mlx_init(800, 600, "Cub3D", true);
 	init_map_images(game);
+	load_screen(game);
 	mlx_loop(game->mlx);
 	return (0);
 }
--- src/renders.c
+++ src/renders.c
@@ -6,11 +6,20 @@
 /*   By: rheringe <rheringe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:44:12 by rheringe          #+#    #+#             */
-/*   Updated: 2025/07/25 17:51:48 by rheringe         ###   ########.fr       */
+/*   Updated: 2025/07/25 18:20:44 by rheringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
+
+static void	render_walls(t_game *game, int i, int j)
+{
+	int tile_size;
+	
+	tile_size = 64;
+	if (game->map->map[i][j] == '1')
+		mlx_image_to_window(game->mlx, game->image->south_wall, j * tile_size, i * tile_size);
+}
 
 void	load_screen(t_game *game)
 {
@@ -18,9 +27,16 @@ void	load_screen(t_game *game)
 	int	j;
 	
 	i = -1;
+	while (i++, game->map->map[i])
+	{
+		j = -1;
+		while (j++, game->map->map[i][j])
+		{
+			if (game->map->map[i][j] == '1')
+				render_walls(game, i, j);
+		}
+		printf("i:%d j: %d\n", i,  j);
+	}
+
 }
 
-void	render_walls(t_game *game, int i, int j)
-{
-	
-}
