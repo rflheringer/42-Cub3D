5fa2a4b65e75d347c180fde5bdee80eeab96c9c0
Roberto Del Franco <robertinhodf@gmail.com>
2025-08-04 16:16:41 -0300

    fixing norm in minimap bonus

--- src/bonus/minimap_bonus/minimap_bonus.c
+++ src/bonus/minimap_bonus/minimap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 14:26:34 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/08/04 16:06:17 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/08/04 16:16:15 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ void	update_minimap(t_game *game)
 		}
 		i++;
 	}
-	mlx_put_pixel(game->raycasting->image, (int)game->player->pos_x, (int)game->player->pos_y, 0xFF0000FF);
+	mlx_put_pixel(game->raycasting->image, (int)game->player->pos_x,
+		(int)game->player->pos_y, 0xFF0000FF);
 }
 
 void	init_minimap(t_game *game)
@@ -48,10 +49,10 @@ void	init_minimap(t_game *game)
 			if (game->map->map[i][j] == '1')
 				mlx_put_pixel(game->raycasting->image, j, i, 0xFF00F0FF);
 			if (ft_strchr("NSWE", game->map->map[i][j]))
-				mlx_put_pixel(game->raycasting->image, (int)game->player->pos_x, (int)game->player->pos_y, 0xFF0000FF);
+				mlx_put_pixel(game->raycasting->image, (int)game->player->pos_x,
+					(int)game->player->pos_y, 0xFF0000FF);
 			j++;
 		}
 		i++;
 	}
-
 }
