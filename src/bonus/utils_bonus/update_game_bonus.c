5a46b39bc36abfb9d92dd40db43e22b1cd650489
Roberto <robertinhodf@gmail.com>
2025-08-14 12:50:54 -0300

    utils_bonus folder norm

--- src/bonus/utils_bonus/update_game_bonus.c
+++ src/bonus/utils_bonus/update_game_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 11:40:07 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/08/12 15:25:18 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/08/14 12:50:32 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ static void	update_game_over(t_game *game)
 	if (game->raycasting->image)
 		mlx_delete_image(game->mlx, game->raycasting->image);
 	game->texture->game_over = mlx_load_png("assets/player/end_game.png");
-	game->image->game_over = mlx_texture_to_image(game->mlx, game->texture->game_over);
+	game->image->game_over = mlx_texture_to_image(game->mlx,
+			game->texture->game_over);
 	mlx_resize_image(game->image->game_over, WIDTH, HEIGHT);
 	mlx_image_to_window(game->mlx, game->image->game_over, 0, 0);
 }
@@ -27,7 +28,8 @@ static void	update_game_win(t_game *game)
 	if (game->raycasting->image)
 		mlx_delete_image(game->mlx, game->raycasting->image);
 	game->texture->game_win = mlx_load_png("assets/player/win_game.png");
-	game->image->game_win = mlx_texture_to_image(game->mlx, game->texture->game_win);
+	game->image->game_win = mlx_texture_to_image(game->mlx,
+			game->texture->game_win);
 	mlx_resize_image(game->image->game_win, WIDTH, HEIGHT);
 	mlx_image_to_window(game->mlx, game->image->game_win, 0, 0);
 }
